12e258aaa740e7ddac0cebfee845b33139ce2b87
Daniel <daniel.zappold@gmx.de>
2023-07-23 16:42:56 +0200

    walking skeleton

--- presentation/web/src/main/kotlin/application.kt
+++ presentation/web/src/main/kotlin/application.kt
@@ -1,26 +1,27 @@
 import org.http4k.core.HttpHandler
 import org.http4k.core.Method.GET
-import org.http4k.core.Method.POST
 import org.http4k.core.Request
 import org.http4k.core.Response
-import org.http4k.core.Status.Companion.CREATED
 import org.http4k.core.Status.Companion.OK
 import org.http4k.routing.bind
+import org.http4k.routing.path
 import org.http4k.routing.routes
 
-val htmlPage = """
-    <html>
-        <body>
-            <h1 style="text-align: center; font-size: 3em;">Hello functional World!</h1>
-        </body>
-    </html>
-""".trimIndent()
 
 val app: HttpHandler = routes(
-    "/greetings" bind GET to ::greetings,
-    "/data" bind POST to ::receiveData,
+    "/todo/{user}/{list}" bind GET to ::showList,
 )
 
-fun receiveData(request: Request): Response = Response(CREATED).body("Received: ${request.bodyString()}")
-
-fun greetings(request: Request): Response = Response(OK).body(htmlPage)
+fun showList(request: Request): Response {
+    val user = request.path("user")
+    val list = request.path("list")
+    val htmlPage = """
+    <html>
+        <body>
+            <h1>Zettai</h1>
+            <p>Here is the list <b>$list</b> of user <b>$user</b></p>
+        </body>
+    </html>
+    """.trimIndent()
+    return Response(OK).body(htmlPage)
+}
