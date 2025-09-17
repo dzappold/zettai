bdfda3f31e77b85ef671030ea669a4c46dae8ccd
Daniel <daniel.zappold@gmx.de>
2025-09-17 19:46:31 +0200

    ⬆️ update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
     alias(libs.plugins.dependency.check)
 
     id("com.github.jk1.dependency-license-report") version "2.9"
-    id("com.autonomousapps.dependency-analysis") version "3.0.3"
+    id("com.autonomousapps.dependency-analysis") version "3.0.4"
 }
 
 dependencies {
